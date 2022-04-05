635896ddb89b5ffe9adc6a2ec61566f48c21a58f
Neron Joseph <njoseph@menervasoftware.com>
2022-04-05 10:22:16 +0530

    Added some basic Docker commands

--- 01_Docker-basic-commands/docker-basic-commands.sh
+++ 01_Docker-basic-commands/docker-basic-commands.sh
@@ -8,3 +8,15 @@ docker --version
 
 # Find more information about Docker host
 docker system info
+
+# Make Docker run during startup 
+sudo systemctl enable docker 
+
+# View the status of Docker on local machine
+sudo systemctl status docker 
+
+# Start docker manually
+sudo dockerd 
+
+# Start docker manually with debug mode 
+sudo dockerd --debug 
